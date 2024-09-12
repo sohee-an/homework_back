731de3b9d96de6cd541eb6881a02e82ee0b95d35
sohee-an <soan32@hanmail.net>
2024-09-12 20:48:09 +0900

    fix:test config

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('postgres', () => ({
-  host: process.env.POSTGRES_HOST || 'localhost',
+  host: process.env.POSTGRES_HOST || 'host.docker.internal',
   port: Number(process.env.POSTGRES_PORT) || 5434,
   database: process.env.POSTGRES_DATABASE || 'postgres',
   username: process.env.POSTGRES_USERNAME || 'postgres',
