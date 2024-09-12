8f6ba4c9061324b08a719c53ed99db9bdf163659
sohee-an <soan32@hanmail.net>
2024-09-12 20:20:22 +0900

    fix:config 수정

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('postgres', () => ({
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
+  port: Number(process.env.POSTGRES_PORT) || 5434,
   database: process.env.POSTGRES_DATABASE || 'postgres',
   username: process.env.POSTGRES_USERNAME || 'postgres',
   password: process.env.POSTGRES_PASSWORD || 'postgres',
